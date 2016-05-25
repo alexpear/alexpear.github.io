15415ec9ce0e2de72ec3f2796b1d73e068c12578
Alex Pear <alexmpear@gmail.com>
2016-05-24 23:41:48 -0400

    Add first draft of unit stat templates

---
+++ eco/Templates.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var Templates = exports.Templates = {
+    infantry: {
+        size: 1,
+        moveInterval: 12,
+        armor: 2,
+        attack: 2
+    },
+    air: {
+        sprite: 'A',
+        moveInterval: 1,
+        armor: 3,
+        attack: 6
+    },
+
+    // TODO: range
+    tower: {
+        sprite: 'T',
+        moveInterval: 9999,
+        armor: 12,
+        attack: 4
+    },
+    evangelion: {
+        sprite: 'E',
+        moveInterval: 12,
+        armor: 12,
+        attack: 6
+    }
+};
