b0cb74d11ccce5c01be7594c61e3039693a71f08
Alex Pear <alexmpear@gmail.com>
2016-05-24 23:37:50 -0400

    Add functions for Coord

--- eco/Coord.js
+++ eco/Coord.js
@@ -5,4 +5,40 @@ module.exports = class Coord {
         this.r = r || -1;
         this.c = c || -1;
     }
+
+    equals (other) {
+        return this.r === other.r && this.c === other.c;
+    }
+
+    is (other) { return this.equals(other); }
+
+    plus (other) {
+        return new Coord(
+            this.r + other.r,
+            this.c + other.c
+        );
+    }
+
+    minus (other) {
+        return new Coord(
+            this.r - other.r,
+            this.c - other.c
+        );
+    }
+
+    distanceTo (other) {
+        return Math.sqrt(
+            Math.pow(this.r - other.r, 2) +
+            Math.pow(this.c - other.c, 2)
+        );
+    }
+
+    isInBounds (rowCount, colCount) {
+        return 0 <= this.r && this.r < rowCount
+            && 0 <= this.c && this.c < colCount;
+    }
+
+    toString () {
+        return '[' + this.r + ',' + this.c + ']';
+    }
 };
