c3f7d1fb7b6f50b1046f66eea9ab9c9623f9aded
Alex Pear <alexmpear@gmail.com>
2016-05-25 14:17:30 -0400

    Add params to constructor

--- eco/Entity.js
+++ eco/Entity.js
@@ -3,10 +3,13 @@
 var Templates = require('./Templates.js');
 
 module.exports = class Entity {
-    constructor () {
-        this.type = 'entity';
-        this.region = null;
-        this.coord = new Coord();
-        this.sprite = '?';
+    constructor (template, coord) {
+        template = template || Templates.infantry;
+
+        this.type = template.name;  // TODO standardize field name
+        this.region = null;  // TODO
+        this.coord = coord || new Coord();
+        this.sprite = template.sprite;
+        this.stepsTillMove = template.moveInterval - 1;
     }
 };
