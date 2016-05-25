665ff8c2f2aeac3d69a9a6380b8a2f2eec9e4fca
Alex Pear <alexmpear@gmail.com>
2016-05-24 22:11:32 -0400

    Add comment and require() call

--- eco/Region.js
+++ eco/Region.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var Util = require('./Util.js');
+
+// NOTE: I like 'Location' better than 'Region',
+// but the browser consideres 'location' a reserved keyword.
+
 module.exports = class Region {
 	constructor () {
 		this.type = NODE_TYPES.region;
