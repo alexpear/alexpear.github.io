c9ed39be0196b15bd9aa28fc18d966159fa9f3e6
Alex Pear <alexmpear@gmail.com>
2016-05-25 17:25:48 -0400

    Elaborate on first draft of Templates

--- eco/Templates.js
+++ eco/Templates.js
@@ -2,29 +2,38 @@
 
 module.exports = {
     infantry: {
+        name: 'infantry',
         size: 1,
+        sprite: '?',
         moveInterval: 12,
         armor: 2,
-        attack: 2
+        attack: 2,
+        range: 1
     },
     air: {
+        name: 'air',
         sprite: 'A',
         moveInterval: 1,
         armor: 3,
-        attack: 6
+        attack: 6,
+        range: 1
     },
 
     // TODO: range
     tower: {
+        name: 'tower',
         sprite: 'T',
         moveInterval: 9999,
         armor: 12,
-        attack: 4
+        attack: 4,
+        range: 2
     },
     evangelion: {
+        name: 'evangelion',
         sprite: 'E',
         moveInterval: 12,
         armor: 12,
-        attack: 6
+        attack: 6,
+        range: 1
     }
 };
