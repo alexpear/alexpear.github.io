971fe2215a440cf02c9371e921328e8ba8074895
Alex Pear <alexmpear@gmail.com>
2016-05-27 00:42:46 -0400

    Add skeleton of run() in Simulation

--- eco/Simulation.js
+++ eco/Simulation.js
@@ -7,6 +7,15 @@ var Simulation = class Simulation {
     constructor (worldState) {
         this.worldState = worldState || new WorldState();
     }
+
+    run (maxSteps, secondsPerStep, logging, pauseAt) {
+        maxSteps = maxSteps || 100;
+
+        return;  // debug
+        while (this.worldState.stepCount < maxSteps) {
+
+        }
+    }
 };
 
 var sim = new Simulation();
