7f63ba51834e3fd6c0667be0c222876a4e38c3ed
Alex Pear <alexmpear@gmail.com>
2016-06-16 15:18:18 -0400

    Remove unused field

--- eco/Entity.js
+++ eco/Entity.js
@@ -9,7 +9,6 @@ module.exports = class Entity {
         template = template || Templates.infantry;
 
         this.type = template.name;  // TODO standardize field name
-        this.region = null;  // TODO
         this.coord = coord || new Coord();
         this.sprite = template.sprite;
         this.stepsTillMove = template.moveInterval - 1;
