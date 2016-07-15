d1a475c84527d25b99faeaaa42a21d02a8c1cf31
Alex Pear <alexmpear@gmail.com>
2016-07-15 19:34:26 -0400

    Standardize indentation

--- eco/Factions.js
+++ eco/Factions.js
@@ -1,16 +1,16 @@
 'use strict';
 
 module.exports = {
-  gaia: {
-      name: 'Gaia',
-      color: 'green'
-  },
-  empire: {
-      name: 'Empire',
-      color: 'blue'
-  },
-  rebels: {
-      name: 'Rebels',
-      color: 'red'
-  }
+    gaia: {
+        name: 'Gaia',
+        color: 'green'
+    },
+    empire: {
+        name: 'Empire',
+        color: 'blue'
+    },
+    rebels: {
+        name: 'Rebels',
+        color: 'red'
+    }
 };
