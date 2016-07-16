858b38b78e1529ab64d93d6d149d95500b3ef5cd
Alex Pear <alexmpear@gmail.com>
2016-07-16 00:52:25 -0400

    Turn off violence logging

--- eco/WorldState.js
+++ eco/WorldState.js
@@ -354,7 +354,6 @@ module.exports = class WorldState {
         if (attacker.attackSucceeds(target)) {
             // Remove defeated entity.
             target.alive = false;
-            console.log('Entity destroyed at ' + target.coord.toString());
             // Updating entities might not work while .forEach()ing over it.
             // this.entities = this.entities.filter(function (entity) {
             //     return entity.alive;
