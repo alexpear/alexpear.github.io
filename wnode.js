15bca80a2b8674a2bd4a6e0b202780dfeff9fe6f
Alex Pear <alex@havenlife.com>
2018-09-04 13:48:14 -0400

    Add rough draft of generic waffle node class

---
+++ wnode.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const fs = require('fs');
+const Yaml = require('js-yaml');
+
+// Waffle Node
+// WAFFLE is a game engine related to the novel 'You' by Austen Grossman.
+// A person, creature, component, or thing is represented here
+// by a WNode or a tree of WNodes.
+
+let WNode = module.exports = class WNode {
+    constructor(name) {
+        // Later: Safety checks, logging
+        this.id = Util.newId();
+
+        if (name) {
+            this.name = name;
+        }
+
+        this.components = [];
+    }
+
+    deepCopy() {
+        let clone = new WNode();
+        Object.assign(clone, this);
+
+        clone.components = this.components.map(component => component.deepCopy());
+        return clone;
+    }
+
+    toString () {
+        return JSON.stringify(this, undefined, '    ');
+    }
+
+    // Format that looks like informal YAML but with props above components.
+    toPrettyString (indent) {
+        indent = Util.default(indent, 0);
+
+        let outString = furtherLine(Util.formatProp(this, 'name'));
+        outString += furtherLine(Util.formatProp(this, 'templateName'));
+
+        const SPECIAL_PROPS = [
+            'name',
+            'templateName',
+            'components'
+        ];
+
+        for (let prop in this) {
+            if (! prop || Util.contains(SPECIAL_PROPS, prop)) {
+                continue;
+            }
+
+            outString += furtherLine(Util.formatProp(this, prop));
+        }
+
+        if (this.components.length === 0) {
+            outString += furtherLine('components: []');
+        }
+        else {
+            outString += furtherLine('components:');
+            for (let component of this.components) {
+                outString += component.toPrettyString(indent + 2);
+                outString += '\n';
+            }
+
+            // outString += furtherLine(']');
+        }
+
+        return outString;
+
+        function furtherLine (text/*, indent*/) {
+            return text ?
+                Util.repeat(' ', indent) + text + '\n' :
+                '';
+        }
+    }
+
+    toYaml () {
+        return Yaml.dump(this);
+    }
+};
