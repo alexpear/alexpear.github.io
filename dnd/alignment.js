2d4148f23c9d1c013c724bcd2c0204608128838d
Alex Pear <alex@havenlife.com>
2018-10-15 22:37:49 -0400

    Add Alignment

---
+++ dnd/alignment.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// similar to alignment.js in hobby/ git repo.
+const ALIGNMENTS = {
+    LG: {
+        lawChaos: -2,
+        darkRadiant: 2
+    },
+    LE: {
+        lawChaos: -2,
+        darkRadiant: -2
+    },
+    CE: {
+        lawChaos: 2,
+        darkRadiant: -2
+    },
+    CG: {
+        lawChaos: 2,
+        darkRadiant: 2
+    },
+    NN: {}
+}
+
+class Alignment {
+    constructor (abbreviation) {
+        this.abbreviation = abbreviation || 'NN';
+    }
+
+    getAxisValues () {
+        // Later, make this function more robust.
+        return ALIGNMENTS[this.abbreviation] || {};
+    }
+}
+
+module.exports = Alignment;
