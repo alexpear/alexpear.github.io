12bc013aba4299c679f895a19f050d2eb6bcb926
Alex Pear <alex@havenlife.com>
2018-10-26 10:45:02 -0400

    Reorder statements

--- battle20/actiontemplate.js
+++ battle20/actiontemplate.js
@@ -10,17 +10,17 @@ class ActionTemplate {
     static example () {
         const template = new ActionTemplate();
 
+        // Range is in meters. It is okay to round it heavily.
+        template.range = 10;
+        template.hit = 4;
+        template.damage = 1;
+
         // Dwarven throwing axe
         template.tags = [
             TAG.Dwarf,
             TAG.Blade,
             TAG.Projectile
         ];
-
-        // Range is in meters. It is okay to round it heavily.
-        template.range = 10;
-        template.hit = 4;
-        template.damage = 1;
 
         return template;
     }
