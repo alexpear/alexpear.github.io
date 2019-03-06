e422491eaf7fe68203c7d8aab5fa0bf8708f5b94
alexpear <alex@havenlife.com>
2019-03-06 01:07:35 -0500

    Add UNSC army tree generator files

---
+++ codices/halo/unsc/battalion.js
@@ -0,0 +1,55 @@
+module.exports = `* output
+1 staticBattalion
+4 slowBattalion
+4 fastBattalion
+2 airBattalion
+
+* childrenof staticBattalion
+halo/unsc/company/staticCompany
+halo/unsc/company/staticCompany
+halo/unsc/company/staticCompany
+halo/unsc/company/staticCompany
+halo/unsc/company/staticCompany
+halo/unsc/company/staticCompany
+halo/unsc/company/staticCompany
+halo/unsc/company/slowCompany
+halo/unsc/company/fastCompany
+halo/unsc/company/airCompany
+
+* childrenof slowBattalion
+halo/unsc/company/slowCompany
+halo/unsc/company/slowCompany
+halo/unsc/company/slowCompany
+halo/unsc/company/slowCompany
+halo/unsc/company/slowCompany
+halo/unsc/company/slowCompany
+halo/unsc/company/slowCompany
+halo/unsc/company/fastCompany
+halo/unsc/company/fastCompany
+halo/unsc/company/airCompany
+
+* childrenof fastBattalion
+halo/unsc/company/fastCompany
+halo/unsc/company/fastCompany
+halo/unsc/company/fastCompany
+halo/unsc/company/fastCompany
+halo/unsc/company/fastCompany
+halo/unsc/company/fastCompany
+halo/unsc/company/fastCompany
+halo/unsc/company/airCompany
+halo/unsc/company/airCompany
+halo/unsc/company/airCompany
+
+* childrenof airBattalion
+halo/unsc/company/airCompany
+halo/unsc/company/airCompany
+halo/unsc/company/airCompany
+halo/unsc/company/airCompany
+halo/unsc/company/airCompany
+halo/unsc/company/airCompany
+halo/unsc/company/airCompany
+halo/unsc/company/airCompany
+halo/unsc/company/airCompany
+halo/unsc/company/airCompany
+
+`;
---
+++ codices/halo/unsc/company.js
@@ -0,0 +1,55 @@
+module.exports = `* output
+1 staticCompany
+4 slowCompany
+4 fastCompany
+2 airCompany
+
+* childrenof staticCompany
+{halo/unsc/squad/staticSquad}
+{halo/unsc/squad/staticCompatibleSquad}
+{halo/unsc/squad/staticCompatibleSquad}
+{halo/unsc/squad/staticCompatibleSquad}
+{halo/unsc/squad/staticCompatibleSquad}
+{halo/unsc/squad/staticCompatibleSquad}
+{halo/unsc/squad/staticCompatibleSquad}
+{halo/unsc/squad/staticCompatibleSquad}
+{halo/unsc/squad/staticCompatibleSquad}
+{halo/unsc/squad/staticCompatibleSquad}
+
+* childrenof slowCompany
+{halo/unsc/squad/slowSquad}
+{halo/unsc/squad/slowCompatibleSquad}
+{halo/unsc/squad/slowCompatibleSquad}
+{halo/unsc/squad/slowCompatibleSquad}
+{halo/unsc/squad/slowCompatibleSquad}
+{halo/unsc/squad/slowCompatibleSquad}
+{halo/unsc/squad/slowCompatibleSquad}
+{halo/unsc/squad/slowCompatibleSquad}
+{halo/unsc/squad/slowCompatibleSquad}
+{halo/unsc/squad/slowCompatibleSquad}
+
+* childrenof fastCompany
+{halo/unsc/squad/fastSquad}
+{halo/unsc/squad/fastCompatibleSquad}
+{halo/unsc/squad/fastCompatibleSquad}
+{halo/unsc/squad/fastCompatibleSquad}
+{halo/unsc/squad/fastCompatibleSquad}
+{halo/unsc/squad/fastCompatibleSquad}
+{halo/unsc/squad/fastCompatibleSquad}
+{halo/unsc/squad/fastCompatibleSquad}
+{halo/unsc/squad/fastCompatibleSquad}
+{halo/unsc/squad/fastCompatibleSquad}
+
+* childrenof airCompany
+{halo/unsc/squad/airSpeedSquad}
+{halo/unsc/squad/airSpeedSquad}
+{halo/unsc/squad/airSpeedSquad}
+{halo/unsc/squad/airSpeedSquad}
+{halo/unsc/squad/airSpeedSquad}
+{halo/unsc/squad/airSpeedSquad}
+{halo/unsc/squad/airSpeedSquad}
+{halo/unsc/squad/airSpeedSquad}
+{halo/unsc/squad/airSpeedSquad}
+{halo/unsc/squad/airSpeedSquad}
+
+`;
