e523891c4208d9e471d6e378b855ef73d8d6860a
alexpear <alex@havenlife.com>
2019-04-19 11:26:23 -0400

    Add skeleton of Sunlight gen

---
+++ codices/sunlight/warband/warband.js
@@ -0,0 +1,33 @@
+module.exports = `
+
+* output
+1 {60people}, {500t}
+0 This theater limits warbands both by player count and by total weight of vehicles & heavy gear (1t = 1000kg)
+0 The weight budget can also be spent on battlefield abilities like air support, artillery missions, and other tricks.
+
+* alias 60people
+
+
+* alias 500t
+1 giantWarMachine
+10 {200t}, {200t}, {100t}
+
+* childrenof giantWarMachine
+{giantPropulsion}
+{hugeGear}
+
+* alias giantPropulsion
+2 treads
+1 wheels
+4 spiderLegs
+1 millipede
+4 bipedal
+4 repulsorlift
+
+
+
+
+
+
+
+`;
