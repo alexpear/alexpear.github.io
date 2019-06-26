f5424bcc44e3bea8867bb4925154e3e28b4704f1
alexpear <alex@havenlife.com>
2019-06-25 23:16:53 -0400

    Add Creature subclass of Thing

---
+++ wnode/creature.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Coord = require('../util/coord.js');
+const Util = require('../util/util.js');
+const Thing = require('./thing.js');
+
+module.exports = class Creature extends Thing {
+    constructor (templateName, coord) {
+        super(templateName, coord);
+
+        // TODO Take worldState or template as a param, and read the template to init this.hp
+        if (! Util.exists(this.hp)) {
+            this.hp = 1;
+        }
+    }
+
+    actions (worldState) {
+        const template = worldState.glossary[this.templateName];
+
+        return template ?
+            template.actions :
+            [];
+    }
+};
+
