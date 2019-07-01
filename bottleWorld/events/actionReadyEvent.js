f6661a5fe7f827a556b84794adebd1cd7dab2bf7
alexpear <alex@havenlife.com>
2019-07-01 17:59:30 -0400

    Add todos

--- bottleWorld/events/actionReadyEvent.js
+++ bottleWorld/events/actionReadyEvent.js
@@ -11,12 +11,17 @@ module.exports = class ActionReadyEvent extends BEvent {
             protagonist
         );
 
+        // TODO should this event store string actionType or string actionId?
         this.actionType = actionType;
     }
 
     resolve (worldState) {
         // TODO Implement.
         // MRB 1: The protagonist takes the action specified by this.actionType
+        // Or, as a fallback, creature.actions()[0]
+        // This may entail converting from string to CreatureTemplate if protagonist is a string.
+        // Should we support both string and obj for this protagonist field? When serializing, we will prefer string. Storing as string seems neat enough...
+        // (Edge case: Creature has no actions. Maybe throw error, since ActionReady shouldn't have been called.)
         // Parameters and targets can be determined randomly, perhaps by Thing.chooseActionDetails() or something.
     }
 };
