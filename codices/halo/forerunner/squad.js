0d3c56b3081f132f0983c00801e58a4c110547a2
alexpear <alex@havenlife.com>
2019-08-16 17:21:27 -0500

    Skeleton of Forerunner squad gen

---
+++ codices/halo/forerunner/squad.js
@@ -0,0 +1,15 @@
+module.exports = `
+* output
+1 sentinelTeam
+
+`;
+
+/*
+Sketching about Forerunner armies
+. Battalions of 500 combatants
+. Air battalions
+  . Sentinels, Ultra Sentinels
+  . Enforcers
+  . Triremes of Soldiers
+
+*/
