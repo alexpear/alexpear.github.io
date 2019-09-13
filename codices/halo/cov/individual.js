58a59894f5aad0f2b2c16608059c4bb8121f06ac
Alex Pear <alexmpear@gmail.com>
2019-09-12 23:44:48 -0400

    Add cov individual basics

---
+++ codices/halo/cov/individual.js
@@ -0,0 +1,23 @@
+module.exports = `
+
+* output
+4 eliteMinor
+4 gruntMinor
+4 {jackal}
+2 brute
+1 drone
+
+* alias gruntLeader
+4 gruntMajor
+1 gruntUltra
+
+* alias jackal
+4 shieldJackal
+4 rifleJackal
+1 skirmisher
+
+* alias eliteLeader
+4 eliteMajor
+1 eliteUltra
+
+`;
