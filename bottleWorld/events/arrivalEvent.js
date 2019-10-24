d06dcb8d18fa6ed1310655a32c22455fcc0cfb94
alexpear <alex@havenlife.com>
2019-10-24 11:47:58 -0400

    Refactor arrival event

--- bottleWorld/events/arrivalEvent.js
+++ bottleWorld/events/arrivalEvent.js
@@ -23,16 +23,21 @@ const ArrivalEvent = module.exports = class ArrivalEvent extends BEvent {
 
         worldState.addThing(arriver, this.coord);
 
-        // TODO bug where arriver has no actions (2019 Oct 24)
-        const actionReadyEvent = new ActionReadyEvent(arriver, arriver.actions()[0].id);
+        const firstAction = arriver.actions()[0];
+
+        // Util.logDebug(`arriver.templateName: ${arriver.templateName}, arriver.template: ${arriver.template}, arriver.constructor.name: ${arriver.constructor.name}, firstAction: ${firstAction}, worldState.glossary.soldier.actions.length: ${worldState.glossary.soldier.actions.length}`);
+
+        if (! firstAction) {
+            return;
+        }
+
+        const actionReadyEvent = new ActionReadyEvent(arriver, firstAction.id);
 
         this.outcomes.push(actionReadyEvent);
 
-        const timeline = worldState.timeline;
-
-        timeline.addEvent(
+        worldState.timeline.addEvent(
             actionReadyEvent,
-            timeline.now() + ArrivalEvent.ACTION_DELAY
+            this.t + ArrivalEvent.ACTION_DELAY
         );
 
         // Later ignore that ACTION_DELAY placeholder in favor of information found in codex templates.
