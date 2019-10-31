7ef6ce737788cabee76cb7885fa633dd1b36dfa3
alexpear <alex@havenlife.com>
2019-10-31 16:16:34 -0400

    Add todos

--- bottleWorld/events/projectileEvent.js
+++ bottleWorld/events/projectileEvent.js
@@ -62,8 +62,25 @@ module.exports = class ProjectileEvent extends BEvent {
         // ArrivalEvents create Creatures and set them up with starting ActionReadyEvents.
         // AREs entail choosing the details/parameters/target of a action and creating a ActionEvent. (You can think of them as 'Choose Action Events')
         // ActionEvents entail creating some consequential other event, such as 1 or more ProjectileEvents. They also place another ActionReadyEvent into the future.
-        // ProjectileEvents may or may not hit their targets. If they do hit, they cause a ImpactEvent (or ExplosionEvent, for explosive weapons etc).
+        // ProjectileEvents may or may not hit their targets. If they do hit, they deal damage, mitigated by armor. The details of this outcome is persisted in the ProjectileEvent. Suggestion: They store a number describing the resultant SP of the target.
+        // Hits by explosive weapons or on explosive objects can cause a ExplosionEvent. Grenades cause a delayed ExplosionEvent regardless of whether they hit. Certain machines also cause EEs when eliminated.
+        // Other consequences that could be modeled in the ProjectileEvent: Injury effects (MRB 2), morale effects from the hit (probably a separate Event that looks back retrospectively? MRB 2 regardless.), probably a specific EliminationEvent or CasualtyEvent.
+        // But for now i could just include the creature-removal logic (upon 0 SP) in ProjectileEvent.resolve()
+
+        // Old:
+        // If they do hit, they cause a ImpactEvent (or ExplosionEvent, for explosive weapons etc).
         // ImpactEvent.resolve() includes determining whether the attack does damage. If so, the target.sp prop is updated and this is persisted in the ImpactEvent. This can cause a CasualtyEvent if the target is out of action.
+        // ^ Old ^
+
+        // Summary of current combat algorithm:
+        // When attacking, attacker selects a random active (ie not eliminated) creature of a different faction.
+        // MRB 1: Totally ignore range. Ignore all concerns of space for now.
+        // The ProjectileEvent rolls to hit. Undecided whether to use WCW ballistics or Battle20 d20 roll. Just do something MRB 1 and quick, and make it modular so i can change the func later. Computationally fast is good too.
+        // MRB 1: Ignore the concept of cover for now.
+        // If hit, apply damage.
+        // MRB 1: Ignore armor and/or resistance for now.
+        // If SP is now 0 or less, set target.active = false
+        // (For MRB 2, note that 10 Projectiles might hit a target in one second, the first bringing the SP to 0, and the rest being redundant. Later check for this scenario here. For now it's fine to just keep decrementing the SP negative and setting false to false.)
 
     }
 };
