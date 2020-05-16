ec88621b2b400034237f912a890676aba71d5745
alexpear <alex@havenlife.com>
2020-05-16 13:18:59 -0400

    Skeleton of Eclipse-inspired modular spaceship system

---
+++ wnode/spacebattles/codex/chassis.js
@@ -0,0 +1,48 @@
+const Chassis = module.exports = {
+    interceptor: {
+        size: 8,
+        weight: 1000,
+        durability: 100,
+        slots: 5
+    },
+    corvette: {
+        size: 8,
+        weight: 1000,
+        durability: 100
+    },
+    frigate: {
+        size: 8,
+        weight: 1000,
+        durability: 100
+    },
+    cruiser: {
+        size: 8,
+        weight: 1000,
+        durability: 100
+    },
+    carrier: {
+        size: 8,
+        weight: 1000,
+        durability: 100
+    },
+    dreadnought: {
+        size: 8,
+        weight: 1000,
+        durability: 100
+    },
+    starbase: {
+        size: 8,
+        weight: 1000,
+        durability: 100
+    },
+    orbital: {
+        size: 8,
+        weight: 1000,
+        durability: 100
+    },
+    monolith: {
+        size: 8,
+        weight: 1000,
+        durability: 100
+    }
+};
