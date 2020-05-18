911ca5a28d027aa65c0ec8e15ee330862f0ead66
alexpear <alex@havenlife.com>
2020-05-17 23:40:19 -0400

    Touch up awesome random ship builder

--- wnode/spacebattles/vessel.js
+++ wnode/spacebattles/vessel.js
@@ -1,18 +1,22 @@
 'use strict';
 
-const Coord = require('../util/coord.js');
-const Util = require('../util/util.js');
-const Thing = require('./thing.js');
+const Chassis = require('./codex/chassis.js');
+const Parts = require('./codex/parts.js');
+const Thing = require('../thing.js');
+const Coord = require('../../util/coord.js');
+const Util = require('../../util/util.js');
+
+const _ = require('lodash');
+const d20 = require('d20');
 
 // Modular spaceship minigame inspired by Eclipse: Second Dawn board game.
-module.exports = class Vessel extends Thing {
+class Vessel extends Thing {
     constructor (chassisTemplate, parts, coord) {
         super(chassisTemplate);
 
-        this.chassis = chassisTemplate;
         this.components = this.components.concat(parts || []);
 
-        if (! this.isLegal()) {
+        if (! this.legal()) {
             throw new Error(`I cannot construct this illegal ship! ${this.debugString()}`);
         }
 
@@ -26,42 +30,119 @@ module.exports = class Vessel extends Thing {
         return JSON.stringify(this.toJson(), undefined, '    ');
     }
 
+    simpleJson () {
+        const summary = {
+            templateName: this.template.name,
+            currentDurability: this.currentDurability
+        };
+
+        summary.components = {};
+
+        for (const c of this.components) {
+            summary.components[c.template.name] = c.template.power || 0;
+        }
+
+        return summary;
+    }
+
+    netPower () {
+        const parts = this.getParts();
+
+        return Util.sum(
+            parts.map(
+                p => p.template.power || 0
+            )
+        );
+    }
+
+    getParts () {
+        return this.components.filter(
+            c => c.template.context === Vessel.ShipPartContext
+        );
+    }
+
     legal () {
-        // TODO
-        // Check how many Parts are among this.components
-        // Compare to number of slots in chassis
-        // Check power consumption total
+        const parts = this.getParts();
+
+        if (parts.length > this.template.slots) {
+            return false;
+        }
+
+        const netPower = this.netPower();
+
+        if (netPower < 0) {
+            return false;
+        }
+
+        const seen = {};
+
+        for (const c of this.components) {
+            if (c.template.context === Vessel.ShipPartContext) {
+                if (seen[c.template.name]) {
+                    // 2 of the same unique part.
+                    return false;
+                }
+
+                seen[c.template.name] = true;
+            }
+        }
+
         // Later: Check tech requirements
-        // Check for multiple of the same unique
+
+        return true;
     }
 
     getInitiative () {
-
+        return this.traitSumFromParts('initiative');
     }
 
     getTravelDistance () {
-
+        return this.traitSumFromParts('travel');
     }
 
     // Hull total + 1
     getDurability () {
-
+        return 1 + this.traitSumFromParts('durability');
     }
 
-    getMissileAttack () {
+    getMissileAttacks () {
+        const attacks = this.getAllAttacks()
+            .filter(
+                a => a.missile
+            );
 
+        return attacks;
     }
 
-    getNormalAttack () {
+    getNormalAttacks () {
+        const attacks = this.getAllAttacks()
+            .filter(
+                a => ! a.missile
+            );
 
+        return attacks;
+    }
+
+    getAllAttacks () {
+        return this.components
+            .filter(
+                c => c.template.attack
+            )
+            .map(
+                c => c.template.attack
+            );
+    }
+
+    getAimingModifier () {
+        return this.traitSumFromParts('aiming');
     }
 
     getShieldPenalty () {
-
+        return this.traitSumFromParts('shieldPenalty');
     }
 
     static example () {
-
+        const ship = Vessel.randomEclipseShip();
     }
 
     static getExampleAttack () {
@@ -74,31 +155,93 @@ module.exports = class Vessel extends Thing {
         };
     }
 
-    traitSumFromParts () {
+    traitSumFromParts (trait) {
         const sum = 0;
 
         for (const c of this.components) {
-            // if it's a part, add it (or zero) to the sum.
-            // TODO
+            if (c.template && c.template[trait]) {
+                sum += c.template[trait];
+            }
         }
 
         return sum;
     }
 
-    rollAttackDice () {
+    // Returns array of effects
+    rollAttackDice (missileMode) {
+        const attacks = missileMode ?
+            this.getMissileAttacks() :
+            this.getNormalAttacks();
 
+        const rolledDice = [];
+
+        for (const attack of attacks) {
+            for (let i = 0; i < attack.dice; i++) {
+                const special = attack.rift ?
+                    'rift' :
+                    undefined;
+
+                rolledDice.push(
+                    this.rollDie(attack.damage, this.getAimingModifier(), special)
+                );
+            }
+        }
+
+        return rolledDice;
+    }
+
+    rollMissileDice () {
+        return this.rollAttackDice(true);
+    }
+
+    rollDie (damage, modifier, special) {
+        const summary = {
+            damage: damage
+            // outcome: undefined
+        };
+
+        const roll = d20.roll(6);
+
+        if (special === 'rift') {
+            // TODO, see face summary in parts.js
+        }
+
+        if (roll === 1) {
+            summary.outcome = DieOutcome.Miss;
+        }
+        else {
+            summary.outcome = roll === 6 ?
+                DieOutcome.CriticalHit :
+                DieOutcome.Number;
+        }
+
+        summary.value = roll + modifier;
+
+        return summary;
     }
 
     getExampleAttackDice () {
         // These are the rolled dice that are available to be assigned to targets.
         return [
             {
-                rolled: 7,
+                outcome: DieOutcome.CriticalHit,
+                value: 6,
                 damage: 2
             },
             {
-                rolled: 2,
+                outcome: DieOutcome.Number,
+                value: 7,
                 damage: 2
+            },
+            {
+                outcome: DieOutcome.Number,
+                value: 3,
+                damage: 2
+            },
+            {
+                outcome: DieOutcome.Miss,
+                value: 1,
+                damage: 1
             }
         ];
     }
@@ -107,13 +250,61 @@ module.exports = class Vessel extends Thing {
     // Returns number of durability points that would be lost by absorbing these dice.
     // No side effects.
     damageFromDice (dice) {
+        const expectedDamage = 0;
+        const shieldPenalty = this.getShieldPenalty();
 
+        for (const die of dice) {
+            if (
+                die.outcome === DieOutcome.CriticalHit ||
+                die.outcome === DieOutcome.Number &&
+                die.value - shieldPenalty >= 6
+            ) {
+                expectedDamage += die.damage;
+            }
+        }
+
+        return expectedDamage;
     }
 
+    // Side effects, returns nothing.
     randomizeParts (techs) {
         techs = techs || [];
 
+        // Remove all existing Part components
+        this.components = this.components.filter(
+            c => (! c.template) || c.template.context !== Vessel.ShipPartContext
+        );
 
+        // TODO: Enable repeats of non-unique parts
+        const partNames = _.shuffle(Object.keys(Parts));
+        let slotsFilled = 0;
+
+        for (const name of partNames) {
+            if (slotsFilled >= this.template.slots) {
+                break;
+            }
+
+            const part = Vessel.makePart(name);
+            delete part.coord;
+
+            this.components.push(part);
+
+            if (this.legal()) {
+                slotsFilled += 1;
+            }
+            else {
+                this.components.pop();
+            }
+        }
+    }
+
+    static makePart (partName) {
+        const template = Parts[partName];
+
+        template.name = partName;
+        template.context = Vessel.ShipPartContext;
+
+        return new Thing(template);
     }
 
     static randomEclipseShip () {
@@ -134,5 +325,24 @@ module.exports = class Vessel extends Thing {
         return vessel;
     }
 
+    static test () {
+        const ship = Vessel.randomEclipseShip();
 
+        Util.logDebug(ship.simpleJson());
+
+        Util.logDebug(`Net Power: ${ship.netPower()}. Legal? ${ship.legal()}`)
+    }
 };
+
+Vessel.ShipPartContext = 'shipPart';
+
+Vessel.DieOutcome = Util.makeEnum([
+    'Miss',
+    'Number',
+    'CriticalHit',
+    'FriendlyFire'
+]);
+
+module.exports = Vessel;
+
+Vessel.test();
