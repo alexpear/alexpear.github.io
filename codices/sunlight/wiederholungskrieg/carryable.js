add9d17cb399377965de835a7b2b77ee7bd4fcf7
alexpear <alex@havenlife.com>
2020-08-12 17:23:07 -0400

    Expand js version of infantry weapons codex

---
+++ codices/sunlight/wiederholungskrieg/carryable.js
@@ -0,0 +1,102 @@
+module.exports = `* output
+4 meleeWeapon
+4 rangedWeapon
+1 bomb
+
+* childrenof meleeWeapon
+{meleeMod}
+{meleeChassis}
+
+* alias meleeChassis
+6 mace
+6 hammer
+1 glove
+1 claw
+10 knife
+10 sword
+20 spear
+4 pike
+4 glaive
+4 halberd
+6 axe
+1 hatchet
+1 whip
+2 flail
+2 javelin
+
+* childrenof rangedWeapon
+{rangedMod}
+{rangedChassis}
+
+* alias rangedChassis
+10 pistol
+15 smg
+15 carbine
+8 shotgun
+15 rifle
+10 longrifle
+3 grenadeLauncher
+2 shortbow
+2 longbow
+4 crossbow
+1 harpoon
+4 repeater
+3 chaingun
+3 cannon
+
+* childrenof bomb
+{bombMod}
+{bombChassis}
+
+* alias bombChassis
+4 grenade
+2 bomb
+
+* alias weaponMod
+20 heavy
+19 lightweight
+8 electric
+5 magnetic
+5 acid
+4 toxin
+5 ion
+4 tracker
+8 fire
+7 thermal
+7 guided
+2 concealed
+1 {softScienceMod}
+
+* alias meleeMod
+30 {weaponMod}
+5 wristMounted
+
+* alias rangedMod
+30 {weaponMod}
+4 wristMounted
+3 rail
+10 laser
+6 beam
+4 plasma
+5 spiderweb
+8 scoped
+9 extendedRange
+6 stealth
+6 bayonet
+
+* alias bombMod
+10 {weaponMod}
+1 laser
+1 plasma
+1 spiderweb
+1 flash
+
+* alias softScienceMod
+9 hardlight
+12 stun
+10 nanite
+3 grav
+2 temporal
+1 void
+
+`;
