57e5e2ba6f35e5b30b085644a6786c7b49182def
alexpear <alex@havenlife.com>
2020-12-12 14:54:57 -0500

    CSV conversion and unit test

--- wnode/cape.js
+++ wnode/cape.js
@@ -8,11 +8,34 @@ const WGenerator = require('../generation/wgenerator.js');
 const Util = require('..//util/util.js');
 
 const fs = require('fs');
+const readline = require('readline');
 
 // For the Cape Demographics fan project.
 class Cape extends Group{
-    constructor () {
+    constructor (csv) {
         super();
+
+        if (csv) {
+            const cells = csv.split(',');
+
+            [
+                this.capeName,
+                this.fullName,
+                this.age,
+                this.gender,
+                this.mbti,
+                this.classification,
+                this.rating,
+                this.theme,
+                this.allegiance
+            ] = cells;
+
+            this.age = Number(this.age);
+            this.rating = Number(this.rating);
+            this.location = cells.slice(9);
+
+            return;
+        }
 
         const wTree = WGenerator.fromCodex('parahumans/cape').getOutputs();
         this.components = wTree[0].components;
@@ -38,9 +61,9 @@ class Cape extends Group{
         ).templateName;
 
         // eg 5
-        this.rating = powerChildren.find(
+        this.rating = Number(powerChildren.find(
             node => node.templateName.length <= 2
-        ).templateName;
+        ).templateName);
 
         const themeNode = this.components.find(node => node.templateName === 'theme');
         this.theme = themeNode && themeNode.components[0].templateName;
@@ -79,10 +102,73 @@ class Cape extends Group{
     }
 
     static withTraits (traitsObj) {
+        // TODO read from txt file instead of EVERYONE.
+        const selected = [];
+        const lineReader = readline.createInterface({
+            input: fs.createReadStream('everyCape.txt')
+        });
+
+        lineReader.on(
+            'line',
+            line => {
+                const cape = new Cape(line);
+
+                for (let key in traitsObj) {
+                    // TODO First attempt to filter into a in-memory array. If too many are ending up in output, stop and write to a out file instead.
+
+                    const skipThese = [];
+                    if (Util.contains(skipThese, key)) {
+                        continue;
+                    }
+
+                    if (key === 'age') {
+                        // Interpret as a maximum for now.
+                        if (cape.age > traitsObj.age) {
+                            return;
+                        }
+
+                        continue;
+                    }
+
+                    if (key === 'rating') {
+                        // Interpret as minimum
+                        if (cape.rating < traitsObj.rating) {
+                            return;
+                        }
+
+                        continue;
+                    }
+
+                    if (key === 'location') {
+                        const path = traitsObj.location;
+
+                        for (let i = 0; i < path.length; i++) {
+                            if (path[i] !== cape.location[i]) {
+                                return;
+                            }
+                        }
+
+                        continue;
+                    }
+
+                    if (! cape[key] || cape[key] !== traitsObj[key]) {
+                        return;
+
+                        // Later support searching by parts of MBTI
+                    }
+                }
+
+                selected.push(cape);
+
+                if (selected.length % 1000 === 0) {
+                    Util.log(`Selected a ${selected.length}th cape.`);
+                }
+            }
+        );
+
         return Cape.EVERYONE.filter(
             cape => {
                 for (let key in traitsObj) {
-                    // TODO read from txt file instead of EVERYONE.
                     // TODO First attempt to filter into a in-memory array. If too many are ending up in output, stop and write to a out file instead.
 
                     const skipThese = [];
@@ -155,36 +241,70 @@ class Cape extends Group{
         );
     }
 
-    static run () {
-        Cape.EVERYONE = [];
-
-        const outStream = fs.createWriteStream(`allCapes-${Util.newId()}.txt`, { flags: 'a' });
-
-        for (let i = 0; i < 683000; i++) {
+    static testCsvConversion () {
+        for (let i = 0; i < 100; i++) {
             const cape = new Cape();
-            // Cape.EVERYONE.push(cape);
+            const csv = cape.toCsvRow();
+            const converted = new Cape(csv);
 
-            // Append to file to save JS memory
-            outStream.write(cape.toCsvRow() + '\n');
+            for (const key in cape) {
+                if (Util.contains(['id', 'components', 'lastVisited'], key)) {
+                    continue;
+                }
 
-            if (i % 50000 === 0) {
-                Util.log(i);
+                const debugMessage = `key ${key} discrepancy (${cape[key]}, type ${typeof cape[key]} vs ${converted[key]}, type ${typeof converted[key]}) in ${csv}`;
+
+                if (key === 'location') {
+                    const loc = cape.location;
+                    const convertedLoc = converted.location;
+                    for (let j = 0; j < loc.length; j++) {
+                        if (loc[j] !== convertedLoc[j]) {
+                            throw new Error(debugMessage);
+                        }
+                    }
+
+                    continue;
+                }
+
+                if (cape[key] !== converted[key]) {
+                    throw new Error(debugMessage);
+                }
             }
         }
+    }
 
-        outStream.end();
+    static run () {
+        Cape.testCsvConversion();
+
+        Cape.EVERYONE = [];
+
+        // const outStream = fs.createWriteStream(`allCapes-${Util.newId()}.txt`, { flags: 'a' });
+
+        // for (let i = 0; i < 683000; i++) {
+        //     const cape = new Cape();
+        //     // Cape.EVERYONE.push(cape);
+
+        //     // Append to file to save JS memory
+        //     outStream.write(cape.toCsvRow() + '\n');
+
+        //     if (i % 50000 === 0) {
+        //         Util.log(i);
+        //     }
+        // }
+
+        // outStream.end();
 
         Util.log('\n' + Cape.toCsv(Cape.EVERYONE));
 
-        Util.log(
-            '\n' +
-            Cape.toCsv(
-                Cape.withTraits({
-                    rating: 6,
-                    location: ['northAmerica', 'usa', 'california', 'santaBarbara']
-                })
-            )
-        );
+        // Util.log(
+        //     '\n' +
+        //     Cape.toCsv(
+        //         Cape.withTraits({
+        //             rating: 6,
+        //             location: ['northAmerica', 'usa', 'california', 'santaBarbara']
+        //         })
+        //     )
+        // );
     }
 };
 
