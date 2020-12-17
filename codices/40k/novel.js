ec67032b9f5ba1a6b900e5bad104bcdc70921b69
alexpear <alex@havenlife.com>
2020-12-17 15:44:58 -0500

    Add specificity

--- codices/40k/novel.js
+++ codices/40k/novel.js
@@ -51,12 +51,52 @@ grasslands
 1 rogueTraders
 1 imperialKnights
 
+* childrenof adeptusAstartes
+{chapter}
+
+* alias chapter
+10 ultramarines
+10 whiteScars
+10 ravenGuard
+10 imperialFists
+10 bloodAngels
+10 darkAngels
+10 ironHands
+10 salamanders
+10 spaceWolves
+8 blackTemplars
+1 silverTemplars
+2 crimsonFists
+1 legionOfTheDamned
+
+* childrenof inquisition
+{ordo}
+
+* alias ordo
+4 ordoHereticus
+4 ordoMalleus
+4 ordoXenos
+
 * alias chaosFaction
 10 traitorLegions
 6 chaosCult
 1 beastmen
 1 chaosKnights
 1 fallenSororitas
+
+* childrenof traitorLegions
+{traitorLegion}
+
+* alias traitorLegion
+10 blackLegion
+10 deathGuard
+10 emperorsChildren
+10 thousandSons
+10 worldEaters
+10 nightLords
+10 ironWarriors
+10 wordBearers
+10 alphaLegion
 
 * alias eldarFaction
 8 craftworldEldar
