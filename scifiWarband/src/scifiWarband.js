b83e2de597504501169ebbe4a4015ca3aad7b0a3
alexpear <alexmpear@gmail.com>
2023-10-28 14:05:36 -0700

    Draft many combat funcs

--- scifiWarband/src/scifiWarband.js
+++ scifiWarband/src/scifiWarband.js
@@ -62,29 +62,77 @@ class ScifiWarband {
         );
     }
 
-    /* Actions
-    Move
-    Take Cover
-    Attack with <weapon>
-    Grab Item (free)
-    Secure Objective
-    First Aid
-    Special Action
-    */
-
     // This function is the mind of the squad.
     // Allowed to return illegal moves.
     chooseActions (curSquad) {
         const sentiments = this.creatures.map(cr => cr.morale());
         // LATER morale can inform chooseActions()
 
+        // A squad may do ONE action (eg move OR attack) each turn.
+        const movePlan = this.desiredMove();
+        const attackPlan = this.desiredAttack();
+        // LATER replace these 2 local vars with array of several possible actions. this.desiredActions() => actionInfo[]
+
+        const roll = Math.random() * (movePlan.desire + attackPlan.desire);
+
+        if (roll <= movePlan.desire) {
+            return [Action.move(curSquad, movePlan.coord)];
+        }
+        else {
+            return [Action.attack(curSquad, attackPlan.target)];
+        }
     }
 
     // If action sequence is illegal, interpret/default to a legal move.
     performActions (actions) {
-        // Roll random chances
-        // Update game state
-        // this.record(events)
+        let nonFrees = 0;
+
+        for (let action of actions) {
+            if (! action.isFree()) {
+                nonFrees += 1;
+                // Only allow 1 nonfree action
+                if (nonFrees >= 2) {
+                    continue; 
+                }
+            }
+
+            this.performAction(action);
+        }
+    }
+
+    performAction (action) {
+        // LATER check for illegal actions.
+
+        const squad = action.subject;
+        const distance = squad.coord.distanceTo(action.target);
+        squad.ready = false;
+
+        if (action.type === Action.TYPE.Move) {
+            if (distance > squad.speed()) {
+                Util.logError(`Illegal action submitted, can't move that far: ${action.toString()}`);
+                // LATER interpret as a more reasonable move.
+                return;
+            }
+
+            squad.coord = action.target;
+            return;
+        }
+
+        if (action.type === Action.TYPE.Attack) {
+            if (! squad.canSee(action.target)) {
+                Util.logError(`Illegal action submitted, can't see target squad: ${action.toString()}`);
+                // LATER interpret as a more reasonable action.
+                return;
+            }
+
+            const events = squad.attack(action.target);
+            this.record(events);
+            this.drawAttack(squad, action.target);
+
+            return;
+        }
+
+        // LATER more action types
     }
 
     setHTML () {
@@ -128,7 +176,8 @@ class ScifiWarband {
 
         for (let thing of this.things) {
             if (thing.team === squad.team) { continue; }
-            
+            if (! squad.canSee(thing)) { continue; }
+
             const dist = squad.distanceTo(thing);
             if (dist < shortestDist) {
                 nearests = [thing];
@@ -171,6 +220,10 @@ class ScifiWarband {
         Util.logDebug(`testNearestFoes() ending test at ${endDate.getUTCMilliseconds()}\n  Total time was ${ms / 1000} seconds, or ${ms / 1000 / this.things.length} seconds per nearestFoes() call.`);
     }
 
+    canSee (otherSquad) {
+        return otherSquad.stealth < this.distanceTo(otherSquad);
+    }
+
     drawSquare (imageURL, x, y) {
         const imgElement = new Image();
         imgElement.src = imageURL;
@@ -208,8 +261,17 @@ class ScifiWarband {
         return this.cornerOfSquare(x) + ScifiWarband.SQUARE_PIXELS * 0.5;
     }
 
+    drawAttack (squad, target) {
+        this.drawAttackXY(
+            squad.coord.x,
+            squad.coord.y,
+            target.coord.x,
+            target.coord.y
+        );
+    }
+
     // Inputs should be in grid coords, not pixel coords.
-    drawAttack (startX, startY, endX, endY) {
+    drawAttackXY (startX, startY, endX, endY) {
         // TODO - draw muzzle flash ray-lines at start
         // TODO line traits & colors
         this.canvasCtx.moveTo(
@@ -278,11 +340,19 @@ class Event {
     }
 
     log () {
-        Util.logEvent(`${this.type}: ${Util.stringify(this.details)}`);
+        Util.logEvent(this.toJson());
     }
 
     toJson () {
+        const safeObj = {
+            timestamp: this.timestamp.getUTCMilliseconds(),
+            type: this.type,
+            details: this.details
+        };
 
+        safeObj.details.target = this.details.target?.id;
+
+        return safeObj;
     }
 
     static encounterStart () {
@@ -302,16 +372,89 @@ class Event {
             }
         )
     }
+
+    static update (t, creature) {
+        return new Event(
+            t, 
+            Event.TYPE.Update,
+            {
+                shieldsTo: creature.shields,
+                cooldownEnds: creature.cooldownEnds,
+            }
+        );
+    }
 }
 
 Event.TYPE = {
     EncounterStart: 'Encounter Start',
     Attack: 'Attack',
+    Update: 'Update',
+};
+
+Event.ATTACK_OUTCOME = {
+    Miss: 'Miss',
+    Cover: 'Stopped by Cover',
+    Hit: 'Hit',
+    // These last few are cool, but we might express these situations via the events created by takeHit() instead.
+    Endured: 'Endured',
+    Damage: 'Damage', // Including shield damage.
+    KO: 'KO',
+};
+
+// TODO Maybe track t globally during battle calculation in Event.t
+// Then dont have to pass it around so much.
+// Altho tricky to store it in 2 places...
+
+class Action {
+    constructor (type, subject, target) {
+        this.type = type;
+        this.subject = subject;
+        this.target = target;
+    }
+
+    static move (subject, coord) {
+        return new Action(Action.TYPE.Move, subject, coord);
+    }
+
+    static attack (subject, target) {
+        return new Action(Action.TYPE.Attack, subject, coord);
+    }
+}
+
+// LATER vaguely contemplating instead using Event to represent planned actions. Downside: Semantically it's a plan, not a event.
+Action.TYPE = {
+    Move: 'Move',
+    TakeCover: 'Take Cover',
+    Attack: 'Attack',
+    Objective: 'Secure Objective',
+    FirstAid: 'First Aid',
+    GrabItem: 'Grab Item',
+    Special: 'Special',
+};
+
+class Item {
+    
+}
+
+// TODO move to Codex.js
+Item.TEMPLATES = {
+    UNSC: {
+        Weapon: {
+            SMG: {
+                type: Creature.ATTACK_TYPE.Kinetic,
+                damage: 1,
+                rof: 4,
+                accuracy: 1, // TODO 
+                preferredRange: 1,
+            }
+        }
+    }
 };
 
 // TODO new file
 class Creature {
     constructor (creatureTemplate) {
+        this.id = Util.uuid();
         this.template = creatureTemplate;
         this.shields = this.template.shields || 0;
         this.cooldownEnds = Infinity;
@@ -326,16 +469,38 @@ class Creature {
 
     // creates Event
     update (t) {
-        // cooldowns, shield regen, etc
         if (this.cooldownEnds <= t) {
             if (this.shields < this.template.shields) {
-                this.shields += 
+                this.shields = this.shields + this.template.shieldRegen;
+
+                if (this.shields >= this.template.shields) {
+                    this.shields = this.template.shields;
+                    this.cooldownEnds = Infinity;
+                }
+
+                return Event.update(t, this);
             }
-
-            this.cooldownEnds = Infinity;
-
         }
-        // Note: Morale checks should probably be initiated at the squad level rather than here
+    }
+
+    speed () {
+        return this.template.speed + (this.status.speed || 0);
+    }
+
+    intrinsicAccuracy () {
+        return this.template.accuracy + (this.status.accuracy || 0);
+    }
+
+    accuracy (weaponTemplate) {
+        return this.intrinsicAccuracy() + (weaponTemplate.accuracy || 0);
+    }
+
+    durability () {
+        return this.template.durability + (this.status.durability || 0);
+    }
+
+    weapon (targetSquad) {
+        return this.template.items[0]; // LATER choose a weapon, or have a better preset system.
     }
 
     // returns number in range [-10, 10]
@@ -344,22 +509,105 @@ class Creature {
         return 5; // Later add complexity
     }
 
-    // Could move this to main encounter class if that makes cover calc easier.
-    // creates Event
-    attack (otherSquad) {
-        // Choose weapon
-        // Roll randomness
-        // save Event
+    // LATER Could move this to main encounter class if that makes cover calc easier.
+    // returns Event[]
+    attack (otherSquad, coverPercent = 0) {
+        const distance = this.squad.distanceTo(otherSquad);
+        const weaponTemplate = this.weapon(); 
+
+        // eg: Melee weapons have a strict max range.
+        if (distance > weaponTemplate.maxRange) {            
+            Util.logEvent(`Creature ${this.id} won't participate in attack because it's beyond this weapon's maxRange: ${weaponTemplate.maxRange}`);
+            return;
+        }
+
+        const advantage = otherSquad.size() * this.accuracy(weaponTemplate);
+        const events = [];
+
+        for (let shot = 1; shot <= Math.ceiling(weaponTemplate.rof || 1); shot++) {
+            const event = Event.attack(this, weaponTemplate, otherSquad);
+            events.push(event);
+
+            if (Math.random() > advantage / (advantage + distance + 1)) {
+                event.details.attackOutcome = Event.ATTACK_OUTCOME.Miss;
+                continue;
+            }
+
+            if (Math.random() <= coverPercent) {
+                event.details.attackOutcome = Event.ATTACK_OUTCOME.Cover;
+                continue;
+            }
+
+            const victim = otherSquad.whoGotHit();
+            event.details.targetCreature = victim;
+            event.details.attackOutcome = Event.ATTACK_OUTCOME.Hit;
+
+            events.push(victim.takeHit(weaponTemplate));
+        }
+
+        return events;
     }
 
     // After considering cover.
-    // creates Event
-    takeHit (projectile) {
-        // Decrease shields & damage
-        // Compare remaining damage to durability
-        // Random effect
-        // Edit this.status
-        // create Event
+    // returns Event
+    takeHit (weaponTemplate, t) {
+        let damage = weaponTemplate.damage;
+
+        if (this.shields) {
+            // TODO decide whether to get t by passing, by a static variable, or set it later.
+            this.cooldownEnds = t + this.template.shieldDelay;
+
+            if (weaponTemplate.attackType === Creature.ATTACK_TYPE.Plasma) {
+                this.shields -= damage * 2;
+
+                if (this.shields < 0) {
+                    damage = Math.abs(this.shields) / 2;
+                    this.shields = 0;
+                    this.takeUnshieldedDamage(damage, weaponTemplate);    
+                }
+            }
+            else {
+                this.shields -= damage;
+
+                if (this.shields < 0) {
+                    damage = Math.abs(this.shields);
+                    this.shields = 0;
+                    this.takeUnshieldedDamage(damage, weaponTemplate);    
+                }
+            }
+        }
+        else {
+            this.takeUnshieldedDamage(damage, weaponTemplate);    
+        }
+
+        // Looks at this.shields, this.cooldownEnds maybe, this.status
+        const event = Event.hit(this, weaponTemplate, t);
+        return event;
+    }
+
+    takeUnshieldedDamage (damage, weaponTemplate) {
+        const resistance = this.template.resistance[weaponTemplate.attackType];
+        if (resistance) {
+            damage *= resistance;
+        }
+
+        const harm = damage / ((damage + this.durability) * Math.random());
+
+        if (harm < 1) { return; }
+
+        const harmedTrait = Util.randomOf(['speed', 'accuracy', 'durability']);
+
+        if (this.status[harmedTrait]) {
+            this.status[harmedTrait] -= Math.floor(harm);
+        }
+        else {
+            this.status[harmedTrait] = Math.floor(harm) * -1;
+        }
+
+        if (this.template[harmedTrait] + this.status[harmedTrait] < 0) {
+            // Too much damage in 1 category causes KO.
+            this.status.ko = true;
+        }
     }
 
     static example () {
@@ -371,30 +619,46 @@ class Creature {
     }
 }
 
-// TODO move this and Squad.TEMPLATES to a Codex.js file
+// TODO move this and Squad.TEMPLATES to a WarbandTemplates.js file
+// TODO resturcutre - WarbandTemplates.UNSC.Creature.Marine
 Creature.TEMPLATES = {
     Marine: {
-        size: 2, 
+        size: 2,
         speed: 1, 
         durability: 10,
+        accuracy: 1, // Later finalize how this calc works.
+        resistance: {},
+        items: [Item.TEMPLATES.UNSC.Weapon.SMG]
     },
-    // LATER How does damage work for: Scorpion, Scarab, UNSC Frigate?
+    // Motive for accuracy stat - Spartans better with firearms than Grunts, also makes takeUnshieldedDamage() status effects simpler.
+    // LATER How does damage work for: Scorpion, Scarab, UNSC Frigate? Based on status debuffs?
+};
+
+Creature.ATTACK_TYPE = {
+    Kinetic: 'Kinetic',
+    Plasma: 'Plasma',
+    Impact: 'Impact',
+    Explosive: 'Explosive',
+    Hardlight: 'Hardlight',
+    Electric: 'Electric',
 };
 
 // TODO new file.
 class Squad {
     constructor (squadTemplate, team, coord) {
+        this.id = Util.uuid();
         this.template = squadTemplate;
         this.team = team;
         this.coord = coord || new Coord();
         this.ready = true;
+        this.resetStealth();
         this.creatures = [];
 
         // Note that this is how you create a homogenous squad from a template. LATER, might often have heterogenous squads coming from customization choices or from a save file.
         for (let i = 1; i <= this.template.quantity; i++) {
-            this.creatures.push(
-                new Creature(this.template.creature)
-            );
+            const cr = new Creature(this.template.creature);
+            this.creatures.push(cr);
+            cr.squad = this;
         }
     }
 
@@ -404,13 +668,68 @@ class Squad {
         );
     }
 
+    activeCreatures () {
+        return this.creatures.filter(cr => ! cr.isKO())
+    }
+
+    size () {
+        return Util.sum(
+            this.activeCreatures().map(cr => cr.template.size)
+        );
+    }
+
+    speed () {
+        return Math.min(
+            this.activeCreatures().map(cr => cr.speed())
+        );
+    }
+
     // unit: squares
+    // TODO - decide whether to use squares as unit for codex stats (meaning i cant change meters per square) or express in meters.
     distanceTo (otherSquad) {
         return this.coord.distanceTo(otherSquad.coord);
     }
 
     update () {
         this.creatures.map(cr => cr.update());
+        this.resetStealth();
+        this.ready = true;
+    }
+
+    resetStealth () {
+        this.stealth = Math.min(
+            this.activeCreatures.map(cr => cr.template.stealth || 0)
+            // TODO check whether .creatures should be replaced with activeCreatures classwide
+        );  
+    }
+
+    attack (targetSquad) {
+        let manyEvents = [];
+        const creatures = this.activeCreatures();
+        for (let cr of creatures) {
+            const events = cr.attack(targetSquad);
+            manyEvents = manyEvents.concat(events);
+        }
+
+        this.stealth -= creatures.length;
+        // LATER make sure stealth & ready changes are well integrated with event system. Motive: Stealth might need displaying.
+
+        return manyEvents;
+    }
+
+    whoGotHit () {
+        const roll = Math.random() * this.size();
+
+        for (let cr of this.creatures) {
+            roll -= cr.template.size;
+
+            if (roll <= 0) {
+                return cr;
+            }
+        }
+
+        Util.logDebug(`Squad.whoGotHit() default case happened for Squad ${this.id}.`);
+        return this.creatures[this.creatures.length - 1];
     }
 
     imageURL () {
