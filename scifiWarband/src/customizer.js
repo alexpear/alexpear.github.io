e9ca4677506bf957b42580da76b1d242a794c2d1
alexpear <alexmpear@gmail.com>
2023-11-05 10:31:34 -0800

    Customizer UI button generation

--- scifiWarband/src/customizer.js
+++ scifiWarband/src/customizer.js
@@ -17,26 +17,59 @@ class Customizer {
 
     initUI () {
         window.customizer = this;
-
+        this.overviewPane = document.getElementById('companyOverview');
         this.setUI();
     }
 
     setUI () {
         for (let company of this.companies) {
-
+            this.addButton(company);
 
             for (let squad of company.squads) {
-
+                this.addButton(squad);
 
                 for (let creature of squad.creatures) {
-
+                    this.addButton(creature);
 
                     for (let item of creature.items) {
-                        
+                        this.addButton(item);
                     }
                 }
             }
         }
+    }
+
+    addButton (component) {
+        const INDENT_BASE = 20;
+
+        const INDENT = {
+            Company: 0,
+            Squad: 1,
+            Creature: 2,
+            Item: 3
+        };
+
+        const button = this.buttonForComponent(component);
+        const pixels = INDENT_BASE * INDENT[component.constructor.name];
+        button.setAttribute('style', `margin-left: ${pixels}px;`);
+
+        this.overviewPane.append(button);
+    }
+
+    buttonForComponent (component) {
+        const button = document.createElement('button');
+        button.setAttribute('class', 'component');
+        // LATER - better to store these values as props of JS obj, or as HTML attrs?
+        button.component = component;
+        button.componentType = component.constructor.name;
+        button.innerHTML = component.name();
+        return button;
+    }
+
+    setCompanies (companies) {
+        this.companies = companies;
+        this.companies.map(c => c.nameSquads());
+        this.initUI();
     }
 
     static exampleCompanies () {
@@ -51,7 +84,7 @@ class Customizer {
         // Customizer.test();
 
         const cmizer = new Customizer();
-        cmizer.companies = Customizer.exampleCompanies();
+        cmizer.setCompanies(Customizer.exampleCompanies());
 
     }
 }
