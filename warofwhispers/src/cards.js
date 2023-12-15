4e1ed84bf510fa54b564a930967cd73fc50faa71
alexpear <alexmpear@gmail.com>
2023-12-15 09:36:40 -0800

    Put cards into rows in html

--- warofwhispers/src/cards.js
+++ warofwhispers/src/cards.js
@@ -106,38 +106,38 @@ class Card {
         const EFFECTS = {
             Single: {
                 bear: 'After your attack, you may make an additional attack with any surviving troops.',
-                eagle: 'Move the active agent to any empty Hive square & then take that square\'s action immediately.',
+                eagle: 'Move the active agent to any empty hive square & immediately take that square\'s action instead. Then proceed from after where it was.',
                 elephant: 'Add 1 troop to your attack.',
-                lion: 'Instead of using the current agent\'s Hive square action, use a Hive square action held by one of your other agents.',
-                horse: 'Move 1 battle group of the active Hive to any other location that Hive controls.',
+                lion: 'Instead of using your current hive square action, use a hive square action controlled by one of your other agents.',
+                horse: 'Choose 2 locations controlled by the active hive. Move any number of troops between them.',
             },
             Double: {
-                bear: 'After your attack, kill all troops involved in that attack.',
+                bear: 'After your attack, kill ALL troops involved in that attack (on both sides).',
                 eagle: 'Your attack may target any empty enemy location.',
                 elephant: 'Add 3 troops to your attack.',
                 lion: 'Add 3 friendly troops to any 1 Harbinger Knowledge location.',
-                horse: 'Rearrange any Hive\'s troops within its controlled locations. (Locations must not be abandoned, & the results must not exceed supply limits.)',
+                horse: 'Rearrange any hive\'s troops within its controlled locations. (Do not abandon a location or exceed supply.)',
             },
             Combo: {
-                beareagle: 'Até: Kill 3 troops in any 1 location.', // mercenaries
-                bearelephant: 'Operation Baskerville: Add 3 friendly troops to any empty location(s).', // recruit
-                bearhorse: 'The Prince Speaks: Take all troops in any location (abandoning it) & move them to any number of adjacent locations controlled by that Hive.', // disperse
-                bearlion: 'Peacewash: After your attack, for each enemy troop killed, add 1 troop of the active Hive to any location the active Hive controls.', // subjugate
-                eagleelephant: 'Gorgons: Choose 1 troop of the active Hive. Kill up to 5 enemy troops in any adjacent locations.', // alchemist's fire
+                beareagle: 'Atë: Kill up to 3 troops in any 1 location.', // mercenaries
+                bearelephant: 'Red Crystal: Add 3 friendly troops to any empty location(s).', // recruit
+                bearhorse: 'The Prince Speaks: Take all troops in any location (abandoning it) & move them to any number of adjacent locations controlled by that hive.', // disperse
+                bearlion: 'Peacewash: After your attack, for each enemy troop killed, add 1 troop of the active hive to any location the active hive controls.', // subjugate
+                eagleelephant: 'Gorgons: Choose 1 troop of the active hive. Kill up to 5 enemy troops in any adjacent locations.', // alchemist's fire
                 eaglehorse: 'World Civil War: Your attack may target any location on the map.', // armada
-                eaglelion: 'Servicers: Add a troop to each location the active Hive controls.', // populate
-                elephanthorse: 'Red Crystal: Add 4 friendly troops to any empty location that has no Harbinger Knowledge or Fortified Position.', // uprising
-                elephantlion: 'World-Ringing River: Add up to 6 troops to the current attack from any location the active Hive controls.', // reinforce
+                eaglelion: 'Servicers: Add a troop to each location the active hive controls.', // populate
+                elephanthorse: 'Operation Baskerville: Add 4 friendly troops to any empty location that has no Harbinger Knowledge or Fortified Position.', // uprising
+                elephantlion: 'World-Ringing River: Add up to 6 troops to the current attack from any location the active hive controls.', // reinforce
                 horselion: 'Antisleep: After performing an action with an agent, take the same action again.', // midnight oil
             },
         };
 
         /* Terminology for TI
         banner -> troop
-        army -> battle group
+        army -> battle group (unused)
         region -> location
-        empire -> Hive
-        council position -> Hive square
+        empire -> hive
+        council position -> hive square
         */
 
         if (animal2) {
@@ -156,7 +156,9 @@ class Card {
 
     static createCards (theme) {
         theme = theme || 'terraignota';
-        const row1 = document.getElementById('row1');
+        const table = document.getElementById('main');
+        let row;// = document.getElementById('row1');
+        let colNum = 99;
 
         for (let animal1 of Card.animals()) {
             for (let animal2 of Card.animals()) {
@@ -164,7 +166,15 @@ class Card {
 
                 const card = new Card(theme, animal1, animal2);
                 const td = card.html();
-                row1.appendChild(td);
+
+                if (colNum > 3) {
+                    row = Util.htmlElement('tr');
+                    table.appendChild(row);
+                    colNum = 1;
+                }
+
+                row.appendChild(td);
+                colNum++;
             }
         }
     }
