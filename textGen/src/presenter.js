3319daa8f0333f9fca80ab7abf0c3fadf74dfd2c
alexpear <alexmpear@gmail.com>
2024-04-18 17:31:32 -0700

    Can use URL to link to a specific generator

--- textGen/src/presenter.js
+++ textGen/src/presenter.js
@@ -12,6 +12,14 @@ const Titles = require('./wildbowTitles.js');
 const Util = require('../../util/util.js');
 
 class Presenter {
+    constructor () {
+        this.dropdownElement = document.getElementById('genDropdown');
+        this.descElement     = document.getElementById('description');
+
+        this.setDropdownFromURL()
+        this.setHTML();
+    }
+
     generators () {
         return {
             bionicle: Bionicle,
@@ -28,19 +36,40 @@ class Presenter {
 
     static init () {
         window.presenter = new Presenter();
-        window.presenter.setHTML();
+    }
+
+    setDropdownFromURL () {
+        const params = new URLSearchParams(window.location.search);
+        // TODO test no-param case, use ?. if needed
+        const startValue = params.get('gen');
+
+        Util.logDebug({
+            dropdownElement: this.dropdownElement,
+            options: this.dropdownElement.options,
+            params,
+            startValue,
+        });
+
+        const optionsArray = Array.from(this.dropdownElement.options);
+        
+        const thatOption = optionsArray.find(
+            op => op.value === startValue
+        );
+
+        // BTW - These parens are for clarity, not necessity.
+        if ((! startValue) || (! thatOption)) { return; }
+
+        thatOption.setAttribute('selected', 'selected');
     }
 
     setHTML () {
         // console.log(`top of setHTML()`);
 
-        const dropdownElement = document.getElementById('genDropdown');
-        const gen = this.generators()[dropdownElement.value];
+        const gen = this.generators()[this.dropdownElement.value];
 
-        // console.log(`dropdown.value is ${dropdownElement.value} & gen is ${gen.constructor.name} & !!gen.randomItem is ${!! gen.randomItem}`);
+        // console.log(`dropdown.value is ${this.dropdownElement.value} & gen is ${gen.constructor.name} & !!gen.randomItem is ${!! gen.randomItem}`);
 
-        const descElement = document.getElementById('description');
-        descElement.innerHTML = gen.outputHTML();
+        this.descElement.innerHTML = gen.outputHTML();
     }
 
     static run () {
