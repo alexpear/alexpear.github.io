27b14e09730d6de3ebe2ae8b4cc4a7c52a83d1a1
alexpear <alexmpear@gmail.com>
2024-07-08 22:45:18 -0700

    Rough draft of parsing Stats page

--- util/fandomscraper/fandomscraper.js
+++ util/fandomscraper/fandomscraper.js
@@ -7,8 +7,6 @@ const https = require('node:https');
 const path = require('path');
 
 class FandomScraper {
-
-
     static run () {
         if (! process.argv ||
             ! process.argv[0] ||
@@ -25,7 +23,11 @@ class FandomScraper {
         if (fs.existsSync(wikiDirPath)) {
             // A directory for this wiki already exists here.
 
-            if (fs.globSync(wikiName + '/*.html')) {
+            const fileNames = fs.readdirSync(wikiDirPath);
+
+            if (fileNames.some(
+                name => name.endsWith('.html')
+            )) {
                 return Util.logError(`HTML files for ${wikiName}.fandom.com are already downloaded. Stopping.`);
             }
         }
@@ -34,33 +36,63 @@ class FandomScraper {
         }
 
         // Read this wiki's Statistics page
+        const chunks = [];
+
         const statsRequest = https.request(
-            `https://${wikiName}.fandom.com/Special:Statistics`,
+            `https://${wikiName}.fandom.com/wiki/Special:Statistics`,
             {},
             response => {
                 console.log(`STATUS: ${response.statusCode}`);
                 console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
 
-                res.setEncoding('utf8');
+                response.setEncoding('utf8');
 
-                res.on('data', (chunk) => {
-                    console.log(`BODY: ${chunk}`);
+                response.on('data', (chunk) => {
+                    chunks.push(chunk);
                 });
 
-                res.on('end', () => {
-                    console.log('No more data in response.');
+                response.on('end', () => {
+                    Util.log('No more data in response.');
+
+                    FandomScraper.parseStatisticsResponse(chunks);
                 });
             },
         );
 
-        request.on('error', (e) => {
+        statsRequest.on('error', (e) => {
             throw new Error(e);
         });
 
-        req.end();
+        statsRequest.end();
 
-        // TODO find the link to latest xml in the Statistics response.
+        // Find the link to latest xml in the Statistics response.
+        const ID_STRING = "id='mw-input-wp1'";
 
+        const responseString = chunks.join('');
+
+        Util.logDebug({
+            wp1Index: responseString.indexOf('mw-input-wp1'),
+            anyHrefIndex: responseString.indexOf('href'),
+            responseStart: responseString.slice(0, 9999),
+        });
+
+        const beforeAfterId = responseString.split(ID_STRING);
+
+        // Then look for the next 'href' after that.
+        const afterHref = beforeAfterId[1]
+            .split('href')[1]
+            .trim();
+
+        const urlPart = afterHref.slice(2);
+        const endQuoteIndex = urlPart.indexOf('"');
+        const url = urlPart.slice(0, endQuoteIndex);
+
+        Util.log(url);
+
+    }
+
+    static parseStatisticsResponse (chunks) {
+        // TODO
     }
 }
 
