e72394a696d9ca9ed8bf559da93f737ed0a0c1da
alexpear <alexmpear@gmail.com>
2024-07-26 15:14:57 -0700

    Digital counterpart of multigenre card game

---
+++ bottleWorld/waffleCards/src/build/parser.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// This file is run when building the Waffle Cards software.
+// It writes cards.js. Users should edit cards.yml as the source of truth.
+// cards.js will be a generated file that the software interacts with, & that browserify can bundle.
+
+const Util = require('../../../util/util.js');
+const fs = require('fs');
+// const path = require('path');
+const yaml = require('js-yaml');
+
+class Parser {
+
+
+    static run () {
+
+    }
+}
+
+Parser.run();
---
+++ bottleWorld/waffleCards/src/waffleCards.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Digital prototype of omni-genre card game system
+
+// const AllCards = require('./cards.yml');
+const Util = require('../../util/util.js');
+
+class Card {
+    constructor () {
+
+    }
+
+    asHtml () {
+        return ``;
+    }
+
+    static random () {
+
+    }
+}
+
+class Cardset {
+    constructor (cards) {
+        this.cards = cards || [];
+    }
+
+
+}
+
+// Writes a file cardSet.html that displays 1+ cards.
+class CardTableau {
+
+    static demo () {
+
+    }
+}
+
+CardTableau.demo();
