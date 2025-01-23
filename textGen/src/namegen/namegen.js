4cd3b57462ad0ed2351060785eb09e7f96613165
alexpear <alexmpear@gmail.com>
2025-01-23 12:03:13 -0800

    Weighted phonemes

--- textGen/src/namegen/namegen.js
+++ textGen/src/namegen/namegen.js
@@ -14,66 +14,79 @@ class NameGen extends TextGen {
     }
 
     voweloids () {
-        return [
-            'A',
-            'E',
-            'I',
-            'O',
-            'U',
-            'AI',
-            'OW',
-            'AR',
-            'ER',
-            'EER',
-            'OUR',
-            'ANG',
-            'ENG',
-            'ING',
-            'ONG',
-        ];
+        return {
+            // Each phoneme string has a frequency weight.
+            'A': 2,
+            'E': 2,
+            'I': 2,
+            'O': 2,
+            'U': 2,
+            'AI': 1,
+            'OW': 1,
+            'AR': 1,
+            'ER': 1,
+            'EER': 1,
+            'OUR': 1,
+            'ANG': 1,
+            'ENG': 1,
+            'ING': 1,
+            'ONG': 1,
+        };
     }
 
     consonoids () {
-        return [
-            'B',
-            'D',
-            'F',
-            'G',
-            'H',
-            'J',
-            'K',
-            'L',
-            'M',
-            'N',
-            'P',
-            'R',
-            'S',
-            'T',
-            'V',
-            'W',
-            'Y',
-            'Z',
-            'TS',
-            'TH',
-            'SH',
-            'CH',
-            'FL',
-            'PL',
-            'KY',
-            'NY',
-        ];
+        return {
+            'B': 2,
+            'D': 2,
+            'F': 2,
+            'G': 1,
+            'H': 1,
+            'J': 1,
+            'K': 2,
+            'L': 2,
+            'M': 2,
+            'N': 2,
+            'P': 2,
+            'R': 2,
+            'S': 2,
+            'T': 2,
+            'V': 2,
+            'W': 1,
+            'Y': 1,
+            'Z': 1,
+            'TS': 1,
+            'TH': 2,
+            'SH': 2,
+            'CH': 2,
+            'FL': 1,
+            'PL': 1,
+            'KY': 1,
+            'NY': 1,
+        };
     }
 
     getVoweloid () {
-        return Util.randomOf(this.voweloids());
+        return this.dict2phoneme(this.voweloids());
     }
 
     getConsonoid () {
-        return Util.randomOf(this.consonoids());
+        return this.dict2phoneme(this.consonoids());
+    }
+
+    dict2phoneme (dict) {
+        const phonemes = [];
+
+        for (let phoneme in dict) {
+            for (let i = 0; i < dict[phoneme]; i++) {
+                phonemes.push(phoneme);
+            }
+        }
+
+        return Util.randomOf(phonemes);
     }
 
     wordArray (phonemeCount) {
-        phonemeCount = phonemeCount || Util.randomRange(1, 10);
+        phonemeCount = phonemeCount || Util.randomRange(2, 10);
 
         const array = Math.random() < 0.5 ?
             [ this.getConsonoid() ] :
