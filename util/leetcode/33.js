b7c27ca396fd94a9d3ef9e48fd9446f8ffebc120
alexpear <alexmpear@gmail.com>
2025-02-04 11:40:35 -0800

    Draft some ideas that i will abandon

--- util/leetcode/33.js
+++ util/leetcode/33.js
@@ -9,39 +9,56 @@ var search = function(nums, target) {
     let maxi = nums.length - 1;
     let mink = 0;
     let maxk = nums.length - 1;
-    let spotlight = Math.floor(nums.length / 2);
+    let spotlight = Math.floor(nums.length / 2);  // 0;
     let prevspotlight;
 
     // Or can we do 2 passes in log n? Find pivot, find target?
     // Maybe we dont need to find the pivot, & can do spotlight % nums.length to handle wrap.
 
-    while (true) {
-        if (prevspotlight === spotlight) {
-            return -1;
-        }
-        else {
-            prevspotlight = spotlight;
-        }
+    while (prevspotlight !== spotlight) {
+        prevspotlight = spotlight;
 
-        if (nums[spotlight] === target) {
+        const currentValue = nums[spotlight % nums.length];
+
+        if (currentValue === target) {
             return spotlight;
         }
-        else if (nums[spotlight] < target) {
-            // if prevspotlight was left or right of spotlight, whichever is weirder, then set one of the mink maxk fields.
-            if (spotlight < prevspotlight) {
-                // TODO is this enough info, or does it conflate overshooting up a slope with crossing the pivot?
+        else if (currentValue < target) {
+            // Obstacle: If you overshoot right & pass the pivot, when should you go left, & when right?
+            // Also will be risk of repeat visits, unless we do a tabu list.
+            // Maybe should avoid shifting spotlight outside the range [-length, 2 * length]
 
-            }
+            // mini & maxi are on the spotlight number line
+            // Keep bringing them closer together
+            // Always go to the midpoint between them
+            // When adjacent, return -1
+            // If you pass the pivot, detect that somehow
+            // Then try to express mini & maxi such that mini < maxi
+
+            // Or, rather, detect maxi < mini & then increment maxi += nums.length to fix it.
+            // Can that extend our runtime infinitely? TODO
+
+
+            // if prevspotlight was left or right of spotlight, whichever is weirder, then set one of the mink maxk fields.
+            // if (spotlight < prevspotlight) {
+            //     // TODO is this enough info, or does it conflate overshooting up a slope with crossing the pivot?
+
+            // }
             mini = spotlight;
-            spotlight += Math.floor((maxi - spotlight) / 2);
         }
         else {
             maxi = spotlight;
-            spotlight -= Math.ceil((spotlight - mini) / 2);
         }
+
+        if (maxi < mini) {
+            // Expressed in terms of spotlight's number line, before the % operator.
+            maxi += nums.length;
+        }
+
+        spotlight = Math.floor( (mini + maxi) / 2 );
     }
 
-    // return -1;
+    return -1;
 };
 
 const TESTS = [
