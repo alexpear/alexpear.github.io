aa195e604e42ad999e089ca790b83fe8ec2caa67
alexpear <alexmpear@gmail.com>
2025-02-04 13:15:34 -0800

    Practise 33 has too sensitive pivot detection. Describe plan.

--- util/leetcode/33.js
+++ util/leetcode/33.js
@@ -5,19 +5,29 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let mini = nums[0] < target ?
-        -1 * nums.length :
-        -1 * nums.length + 1;
+    if (nums.length <= 1) {
+        return nums[0] === target ?
+            0 :
+            -1;
+    }
+
+    let mini;
+    if (nums[0] < target) {
+        mini = -1 * nums.length;
+    }
+    else if (nums[1] === target) {
+        // We have to check if nums[1] is target so that the while loop can exit confidently later.
+        return 1;
+    }
+    else {
+        // Corresponds to nums[1].
+        mini = -1 * nums.length + 1;
+    }
 
     let maxi = nums.length;
-    // let mink = 0;
-    // let maxk = nums.length - 1;
     let spotlight = 0; //Math.floor(nums.length / 2);
     let prevSpotlight;
     let prevValue;
-
-    // Or can we do 2 passes in log n? Find pivot, find target?
-    // Maybe we dont need to find the pivot, & can do spotlight % nums.length to handle wrap.
 
     while ( (maxi - mini) >= 2 ) {
 
@@ -32,6 +42,8 @@ var search = function(nums, target) {
         if (currentValue === target) {
             return normalized;
         }
+        // TODO replace these 2 ifs. Instead, detect if you're in a Bad Interval. Defined as, if you would gradient descent in it, you would move towards either mini or maxi, & you know the value there is not good enough. So instead you have to jump towards the other one (mini or maxi) to get out of the Bad Interval.
+        // If not in a bad interval, do normal gradient descent.
         else if (prevSpotlight < spotlight && currentValue < prevValue) {
             // We went right but values decreased. We passed the pivot.
             maxi = spotlight;
@@ -41,26 +53,6 @@ var search = function(nums, target) {
             mini = spotlight;
         }
         else if (currentValue < target) {
-            // Obstacle: If you overshoot right & pass the pivot, when should you go left, & when right?
-            // Also will be risk of repeat visits, unless we do a tabu list.
-            // Maybe should avoid shifting spotlight outside the range [-length, 2 * length]
-
-            // mini & maxi are on the spotlight number line
-            // Keep bringing them closer together
-            // Always go to the midpoint between them
-            // When adjacent, return -1
-            // If you pass the pivot, detect that somehow
-            // Then try to express mini & maxi such that mini < maxi
-
-            // Or, rather, detect maxi < mini & then increment maxi += nums.length to fix it.
-            // Can that extend our runtime infinitely? TODO
-
-
-            // if prevSpotlight was left or right of spotlight, whichever is weirder, then set one of the mink maxk fields.
-            // if (spotlight < prevSpotlight) {
-            //     // TODO is this enough info, or does it conflate overshooting up a slope with crossing the pivot?
-
-            // }
             mini = spotlight;
         }
         else {
@@ -77,7 +69,9 @@ var search = function(nums, target) {
         console.log(JSON.stringify({
             mini,
             maxi,
+            prevSpotlight,
             spotlight,
+            prevValue,
             currentValue,
             normalized,
             // modulo: normalized % nums.length,
@@ -116,9 +110,35 @@ const TESTS = [
         ],
         output: -1,
     },
+
+    {
+        inputs: [
+            [1],
+            1,
+        ],
+        output: 0,
+    },
+
+    {
+        inputs: [
+            [3, 1],
+            1,
+        ],
+        output: 1,
+    },
+
+    {
+        inputs: [
+            [3,4,5,6,1,2],
+            2,
+        ],
+        output: 5,
+    },
 ];
 
 function runTests () {
+    let failures = 0;
+
     for (let test of TESTS) {
         console.log();
         console.log(test.inputs[0].join(' '));
@@ -134,12 +154,16 @@ function runTests () {
         //     }
         // }
 
+        if (! passed) { failures++; }
+
         const shouldBe = passed ?
             `` :
             ` but it should be ${test.output}`;
 
         console.log(`(${passed ? 'PASS' : 'FAIL'}) My output was: ${JSON.stringify(myOutput, undefined, '    ')}${shouldBe}`);
     }
+
+    console.log(`\n${failures} tests failed.`);
 }
 
 runTests();
