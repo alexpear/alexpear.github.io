6c958faaa2aabb78d9d24a84edfd83caa27fbde1
alexpear <alexmpear@gmail.com>
2025-02-13 16:56:35 -0800

    Rough draft of polish()

--- visuals/navatar.js
+++ visuals/navatar.js
@@ -19,14 +19,13 @@ class Navatar {
     // Returns the left half, including the middle column.
     rawHalfGrid () {
         const halfGrid = [...Array(this.width)];
-        const columns = Math.ceil(this.width / 2);
 
         // Maximum number that could be encoded by this grid.
-        const gridMax = Math.pow(2, this.width * columns);
+        const gridMax = Math.pow(2, this.width * this.columns());
         const seed = Math.ceil(this.id) % gridMax;
         let pixelNumber = 0;
 
-        for (let x = 0; x < columns; x++) {
+        for (let x = 0; x < this.columns(); x++) {
             for (let y = 0; y < halfGrid.length; y++) {
                 if (x === 0 && ! halfGrid[y]) {
                     halfGrid[y] = [];
@@ -41,6 +40,10 @@ class Navatar {
         return halfGrid;
     }
 
+    columns () {
+        return Math.ceil(this.width / 2);
+    }
+
     // Returns a binary digit of this.id.
     color (pixelNumber) {
         return Math.floor(
@@ -51,6 +54,65 @@ class Navatar {
     polish () {
         // Define ugly based on number of ortho & diag neighbors
         // See Roam 2025 Feb 13
+        const UGLY_ORTHOS = [true, false, false, false, false];
+        const UGLY_DIAGS = [false, false, true, true, true];
+
+        for (let x = 0; x < this.columns(); x++) {
+            for (let y = 0; y < this.halfGrid.length; y++) {
+
+                if (! this.colorAt(x, y)) {
+                    continue; // If pixel is blank, do not polish it.
+                }
+
+                const neighborGrid = this.neighborGrid(x, y);
+
+                const orthoNeighbors = neighborGrid[0, 1] +
+                    neighborGrid[1, 0] +
+                    neighborGrid[1, 2] +
+                    neighborGrid[2, 1];
+
+                const diagNeighbors = neighborGrid[0, 0] +
+                    neighborGrid[0, 2] +
+                    neighborGrid[2, 0] +
+                    neighborGrid[2, 2];
+
+                if (UGLY_ORTHOS[orthoNeighbors] && UGLY_DIAGS[diagNeighbors]) {
+                    // Visually ugly situation, turn this pixel off.
+                    this.halfGrid[y][x] = 0;
+                }
+            }
+        }
+    }
+
+    neighborGrid (x, y) {
+        const nGrid = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0],
+        ];
+
+        for (let nx = -1; nx <= 1; nx++) {
+            for (let ny = -1; ny <= 1; ny++) {
+                let neighbors = 0;
+
+                // TODO deal with reflections
+                if (
+                    x + nx < 0 ||
+                    x + nx >= this.width ||
+                    y + ny < 0 ||
+                    y + ny >= this.width
+                ) {
+                    neighbors = 0;
+                }
+                else {
+                    neighbors = this.colorAt(x + nx, y + ny) ?
+                        1 : // This grid is colorblind.
+                        0;
+                }
+
+                nGrid[ny + 1][nx + 1] = neighbors;
+            }
+        }
     }
 
     toString (indent) {
@@ -76,21 +138,11 @@ class Navatar {
     }
 
     colorAt (x, y) {
-        const columns = Math.ceil(this.width / 2);
-
-        if (x >= columns) {
+        if (x >= this.columns()) {
             x = this.width - 1 - x;
-
-            // width 5
-            // x = 2 is legit
-            // x = 3 -> 1
-            // x = 4 -> 0
-
-            // width 8
-            // x = 3 is legit
-            // x = 4 -> 3
-            // x = 5 -> 2
         }
+
+        // TODO handle surpassing edges of grid.
 
         return this.halfGrid[y][x];
     }
