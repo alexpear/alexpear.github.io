87f1d9999cd40607cb5cf1c643df42c31b601e9d
alexpear <alexmpear@gmail.com>
2025-03-25 14:49:20 -0700

    Skeleton of facegen - WIP

---
+++ visuals/chargen/src/faceGen.js
@@ -0,0 +1,63 @@
+// Renders simple procgen faces.
+
+class Face {
+    constructor () {
+        // this.eyeType = 
+
+        this.canvas = document.getElementById('faceCanvas')
+            .getContext('2d');
+        // this.ctx = this.canvas.getContext('2d');
+    }
+
+    render () {
+        // LATER output a image. Currently draw to html canvas.
+
+        this.renderEyes();
+        this.renderNose();
+        this.renderMouth();
+        this.renderHair();
+
+        // DEBUG
+        this.canvas.fillStyle = 'blue';
+        this.canvas.fillRect(0, 0, this.canvas.canvas.width, this.canvas.canvas.height);
+
+        this.canvas.fillStyle = 'black';
+        // this.canvas.beginPath();
+        this.canvas.fillRect(50, 50, 50, 50);   //100, 300, 200, 200);
+        // this.canvas.fill();
+        // alert('rect drawn');
+    }
+
+    renderEyes () {
+
+    }
+
+    renderNose () {
+
+    }
+
+    renderMouth () {
+        this.canvas.strokeStyle = 'black';
+        this.canvas.lineWidth = 10;
+        this.canvas.beginPath();
+        this.canvas.moveTo(200, 500);
+        this.canvas.quadraticCurveTo(300, 400, 400, 500);
+        this.canvas.stroke();
+
+        console.log('mouth stroked');
+    }
+
+    renderHair () {
+
+    }
+
+    static run () {
+        window.addEventListener('load', () => {
+            const face = new Face();
+
+            face.render();
+        });
+    }
+}
+
+Face.run();
