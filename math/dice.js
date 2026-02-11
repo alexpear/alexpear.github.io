55037b6a5c1280e5bff2037a90a992d8525b8088
alexpear <alexmpear@gmail.com>
2026-02-11 14:31:22 -0800

    More dice math simulations

--- math/dice.js
+++ math/dice.js
@@ -10,23 +10,45 @@ We are interested in how many steps it takes to reach max level.
 const Util = require('../util/util.js');
 
 class Scenario {
-    constructor (diceCount = 1, stopNumber) {
+    constructor (diceCount = 1, inputLevel, descend = false) {
         this.dice = Number(diceCount);
-        this.level = this.dice;
-        this.stopNumber = stopNumber ?
-            Number(stopNumber) :
+        this.inputLevel = inputLevel ?
+            Number(inputLevel) :
             undefined;
+
+        this.descend = descend;
     }
 
     simulate () {
-    // }
+        return this.descend ?
+            this.simulateDecay() :
+            this.simulateGrowth();
+    }
 
-    // simulateDecay () {
-    // }
-
-    // simulateGrowth () {
-        const maxLevel = this.stopNumber || (6 * this.dice);
+    // Bug - hangs
+    simulateDecay () {
         let steps = 0;
+        this.level = this.inputLevel;
+        const minLevel = this.dice; // All dice showing 1.
+
+        while (this.level > minLevel) {
+            steps += 1;
+
+            const roll = Util.rollXdY(this.dice, 6);
+
+            // TODO also if roll === maxRoll, and for Growth() too.
+            if (roll > this.level) {
+                this.level -= 1;
+            }
+        }
+
+        return steps;
+    }
+
+    simulateGrowth () {
+        let steps = 0;
+        this.level = this.dice; // All dice are set to 1.
+        const maxLevel = this.inputLevel || (6 * this.dice);
 
         while (this.level < maxLevel) {
             steps += 1;
@@ -44,7 +66,7 @@ class Scenario {
     static printHistogram (stepsDict) {
         const tests = Util.sum(Object.values(stepsDict));
 
-        for (let stepCount in stepsDict) {
+        for (const stepCount in stepsDict) {
             const occurrences = stepsDict[stepCount];
             const percentage = ((occurrences / tests) * 100).toFixed(2);
 
@@ -56,21 +78,26 @@ class Scenario {
         // usage
         // node math/dice.js 3 14
         // 3d6, stop at level 14
-        let diceCount = process.argv[2] || 1;
-        let stopNumber = process.argv[3] || undefined;
+
+        // node math/dice.js 3 14 down
+        // 3d6, go down from level 14 to minimum
+
+        const diceCount = process.argv[2] || 1;
+        const inputLevel = process.argv[3] || undefined;
+        const descend = process.argv[4] || false;
 
         const stepsDict = {};
 
         Util.log({
             diceCount,
-            stopNumber,
+            inputLevel,
         });
 
         const startTime = Date.now();
         const duration = 10000; // 10 seconds
 
         while (Date.now() - startTime < duration) {
-            const scenario = new Scenario(diceCount, stopNumber);
+            const scenario = new Scenario(diceCount, inputLevel, descend);
             const steps = scenario.simulate();
 
             if (stepsDict[steps]) {
