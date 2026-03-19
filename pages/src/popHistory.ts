6ede79ae1b9bba4af4e99a94eb76478259052b30
alexpear <alexmpear@gmail.com>
2026-03-19 14:34:31 -0700

    First draft of coord lookup funcs. Mostly copilot.

--- pages/src/popHistory.ts
+++ pages/src/popHistory.ts
@@ -6,7 +6,52 @@ import Util from '../../util/util';
 export class PopHistory {
     coord2census: Record<string, TownCensus[]> = {}; // maps "lat,long" to a list of censuses for that location, sorted by year
 
-    importCensusList(censuses: TownCensus[]): void {}
+    importCensusList(censuses: TownCensus[]): void {
+        const tempCoord2census: Record<string, TownCensus[]> = {};
+
+        for (const census of censuses) {
+            const key = `${census.lat},${census.long}`;
+
+            if (tempCoord2census[key]) {
+                tempCoord2census[key].push(census);
+            } else {
+                tempCoord2census[key] = [census];
+            }
+        }
+
+        for (const key in tempCoord2census) {
+            if (this.coord2census[key]) {
+                this.coord2census[key] = this.coord2census[key].concat(
+                    tempCoord2census[key],
+                );
+            } else {
+                this.coord2census[key] = tempCoord2census[key];
+            }
+
+            // sort this town's censuses by year
+            this.coord2census[key].sort((a, b) => {
+                const difference = a.year - b.year;
+
+                if (difference === 0) {
+                    if (a.confidence >= b.confidence) {
+                        // Mark inferior duplicates
+                        b.confidence = 0;
+                    } else {
+                        a.confidence = 0;
+                    }
+
+                    return 0;
+                }
+
+                return difference;
+            });
+
+            // remove duplicate censuses
+            this.coord2census[key] = this.coord2census[key].filter(
+                (census) => census.confidence !== 0,
+            );
+        }
+    }
 
     townCoordsInBox(
         lat1: number,
@@ -14,8 +59,17 @@ export class PopHistory {
         lat2: number,
         long2: number,
     ): [number, number][] {
-        // TODO
-        return [[0, 0]];
+        // LATER test performance & explore speedups
+
+        return Object.keys(this.coord2census)
+            .map((key) => key.split(',').map(Number) as [number, number])
+            .filter(
+                ([lat, long]) =>
+                    lat >= Math.min(lat1, lat2) &&
+                    lat <= Math.max(lat1, lat2) &&
+                    long >= Math.min(long1, long2) &&
+                    long <= Math.max(long1, long2),
+            );
     }
 
     // The most recent census before the given year.
