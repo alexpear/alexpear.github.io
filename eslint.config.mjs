26840520be0909a7a4bf4715be87259b19884d37
alexpear <alexmpear@gmail.com>
2026-04-27 14:56:09 -0700

    Tell lint that we use a starting _ to mean unused var. claude

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ export default tseslint.config(
             eslintConfigPrettier,
         ],
         rules: {
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
             '@typescript-eslint/explicit-function-return-type': 'warn',
             '@typescript-eslint/typedef': [
                 'warn',
