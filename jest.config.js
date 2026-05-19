d86b94621ff22dcda35e5b3fbd3f45d7e7b58180
alexpear <alexmpear@gmail.com>
2026-05-19 08:56:03 -0700

    Unit tests for item gen. claude

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     transform: {
         '\\.mjs$': 'babel-jest', // Use Babel to transpile .mjs files
         'ya?ml$': 'jest-transform-yaml',
+        'games/limitlessmoon.*\\.ts$': ['ts-jest', { tsconfig: 'games/limitlessmoon/tsconfig.test.json' }],
         '\\.ts$': ['ts-jest', { tsconfig: 'games/blockscout/tsconfig.test.json' }],
     },
     extensionsToTreatAsEsm: ['.test.mjs'], // Treat .test.mjs files as ES Modules
